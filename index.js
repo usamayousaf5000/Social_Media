27d6a0e47ddb9663f400d3522b00a34a7342fe64
Usama Yousaf <osamayousaf29@gmail.com>
2024-10-23 11:41:36 +0500

    first

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const secretKey = 'abcdeghijk';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const users = [
+  {
+    id: 1,
+    email: 'superadmin@example.com',
+    password: 'superadmin',
+    role: 'superadmin',
+    posts: []
+  },
+  {
+    id: 2,
+    email: 'admin@example.com',
+    password: 'admin',
+    role: 'admin',
+    posts: [
+      { id: 1, content: "Admin's post1", likes: [], comments: [] },
+      { id: 2, content: "Admin's post2", likes: [], comments: [] },
+      { id: 3, content: "Admin's post3", likes: [], comments: [] }
+    ]
+  },
+  {
+    id: 3,
+    email: 'user1@example.com',
+    password: 'user1',
+    role: 'user',
+    posts: [
+      { id: 4, content: "User 1's post", likes: [], comments: [] },
+      { id: 5, content: "User 1's post", likes: [], comments: [] },
+      { id: 6, content: "User 1's post", likes: [], comments: [] }
+    ]
+  },
+  {
+    id: 4,
+    email: 'user2@example.com',
+    password: 'user2',
+    role: 'user',
+    posts: [
+      { id: 7, content: "User 2's post", likes: [], comments: [] },
+      { id: 8, content: "User 2's post", likes: [], comments: [] },
+      { id: 9, content: "User 2's post", likes: [], comments: [] }
+    ]
+  }
+];
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+function authenticate(req, res, next) {
+  let token = req.headers.token;
+  if (!token) {
+    return res.status(401).json({
+      message: "Not logged In."
+    })
+  }
+  var decoded = jwt.verify(token, secretKey);
+  console.log(decoded, 'decode')
+  req.user = decoded;
+  next();
+}
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find(u => u.email === email && u.password === password);
+  if (user) {
+    const token = jwt.sign({ email: user.email, }, secretKey);
+    // console.log("Generated token:", token); 
+    return res.status(200).json({
+      message: "Login Successful",
+      token
+    });
+  } else {
+    return res.status(401).json({
+      message: "Invalid Credentials",
+    });
+  }
+});
+app.get('/isLoggedIn', authenticate, (req, res) => {
+  let user = req.user;
+  return res.status(200).json({
+    message: "Welcome",
+    user
+  })
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
